20b4a2c59207160d45d93565e0474a855a7f0a27
ryuimoto <>
2021-05-09 20:26:14 +0900

    seeder e

--- database/seeds/ThreadsTableSeeder.php
+++ database/seeds/ThreadsTableSeeder.php
@@ -13,13 +13,13 @@ class ThreadsTableSeeder extends Seeder
     public function run()
     {        
         for ($i=0; $i < 100; $i++) { 
-
             $run_first_num = mt_rand(0,9);
             $run_last_num = mt_rand(0,9);
+            $run_details_num = mt_rand(0,9);
 
             Thread::create([
                 'name' => config("word.first_name.$run_first_num").config("word.last_name.$run_last_num") ,
-                'details' => str_random(32),
+                'details' => config("word.details.$run_details_num"),
                 'num_of_comments' => null,
                 'momentum' => null,
             ]);
--- resources/views/user/thread.blade.php
+++ resources/views/user/thread.blade.php
@@ -1,10 +1,13 @@
 @extends('templates.user.main')
 @section('style')
-
+    <link rel="stylesheet" href="{{ asset('css/user/thread.css') }}">
 @endsection
 @section('contents')
-    <div>
-        <p>{{ $thread->name }}</p>
+    <div class="thread">
+        <h2 class="thread-name">{{ $thread->name }}</h2>
         <p>{{ $thread->details }}</p>
+    </div>
+    <div class="comments">
+        
     </div>
 @endsection
