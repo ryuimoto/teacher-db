b4ab95191464f7ad3b65698646f446c3b9df12a2
ryuimoto <>
2021-05-17 18:47:30 +0900

    comment seeder e

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -27,12 +27,22 @@ class CommentsTableSeeder extends Seeder
                 $cur_comment_num++;
             }
 
-            Comment::create([
+            $comment = Comment::create([
                 'thread_id' => $run_thread->id,
                 'comment_num' => $cur_comment_num++,
                 'comment_view_id' => str_random(8),
                 'comment' => config("word.details.$run_comment"),
             ]);
+
+            if(($i % 5) == 0){
+                $parent_comments = Comment::where('thread_id',$run_thread->id)->get();
+              
+                if(isset($parent_comments[1])){
+                    $comment->res_comment_num = $parent_comments[1]->comment_num;
+                    $comment->save();
+                }
+         
+            }
 
             dump($cur_comment_num);
         }
