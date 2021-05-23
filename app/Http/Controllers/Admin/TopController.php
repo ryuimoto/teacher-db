f3c78ac7dba94b35bcf465111e697ea59d0ab759
ryuimoto <>
2021-05-23 17:58:17 +0900

    view libraries add

--- app/Http/Controllers/Admin/TopController.php
+++ app/Http/Controllers/Admin/TopController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class TopController extends Controller
 {
     public function index(){
-        return 24345346;
+        return view('admin.top');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::group(['middleware' => 'auth.very_basic'], function() {
 
     Route::prefix('/admin/0523')->group(function () {
         Route::get('','Admin\TopController@index')->name('admin.top');
+        
     });
 
 });
