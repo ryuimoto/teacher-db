76c2ac814e276d5b274b5c9c057816f514ce9a6d
ryuimoto <>
2021-06-04 15:09:54 +0900

    view edit

--- resources/views/admin/threads.blade.php
+++ resources/views/admin/threads.blade.php
@@ -22,18 +22,6 @@
         <div class="row">
             <div class="col-md-12 col-lg-12 col-sm-12">
                 <div class="white-box">
-                    <div class="d-md-flex mb-3">
-                        <h3 class="box-title mb-0">Threads</h3>
-                        <div class="col-md-3 col-sm-4 col-xs-6 ms-auto">
-                            <select class="form-select shadow-none row border-top">
-                                <option>March 2021</option>
-                                <option>April 2021</option>
-                                <option>May 2021</option>
-                                <option>June 2021</option>
-                                <option>July 2021</option>
-                            </select>
-                        </div>
-                    </div>
                     <div class="table-responsive">
                         <table class="table no-wrap">
                             <thead>
