a936d3db82391e19c29e453e9265f1db636b9458
ryuimoto <>
2021-06-04 15:09:02 +0900

    admint top delete request count add

--- app/Http/Controllers/Admin/TopController.php
+++ app/Http/Controllers/Admin/TopController.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\RequestForDelete;
+
 class TopController extends Controller
 {
     public function index(){
-        return view('admin.top');
+        $request_for_delete_count = RequestForDelete::get()->count();
+
+        return view('admin.top')->with([
+            'request_for_delete_count' => $request_for_delete_count,
+        ]);
     }
 }
