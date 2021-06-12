a35f7f88c1808225436b0f4100549ea0b6a3417a
ryuimoto <>
2021-06-12 17:38:34 +0900

    e

--- resources/views/admin/thread.blade.php
+++ resources/views/admin/thread.blade.php
@@ -26,7 +26,7 @@
                             <div class="form-group mb-4">
                                 <label class="col-md-12 p-0">Details</label>
                                 <div class="col-md-12 border-bottom p-0">
-                                    <textarea rows="5" class="form-control p-0 border-0">{{ config("word.details.1"), }}</textarea>
+                                    <textarea rows="5" class="form-control p-0 border-0">{{ $thread->details }}</textarea>
                                 </div>
                             </div>
                             <div class="form-group mb-4">
