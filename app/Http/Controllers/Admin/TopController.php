b1dad160519a84ca1de3b82fdd7fb7a04b35c73a
ryuimoto <>
2021-06-12 14:26:00 +0900

    admin dashboard e

--- app/Http/Controllers/Admin/TopController.php
+++ app/Http/Controllers/Admin/TopController.php
@@ -6,14 +6,17 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\RequestForDelete;
+use App\Thread;
 
 class TopController extends Controller
 {
     public function index(){
         $request_for_delete_count = RequestForDelete::get()->count();
+        $num_of_threads = Thread::get()->count();
 
         return view('admin.top')->with([
             'request_for_delete_count' => $request_for_delete_count,
+            'num_of_threads' => $num_of_threads,
         ]);
     }
 }
