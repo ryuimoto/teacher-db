2410b7fcbba5977c136fc78ea36db7e1f7f5a83a
ryuimoto <>
2021-07-09 15:11:59 +0900

    Prepare a thread of interest

--- app/Http/Controllers/User/TopController.php
+++ app/Http/Controllers/User/TopController.php
@@ -5,9 +5,17 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Thread;
+
 class TopController extends Controller
 {
     public function index(){
-        return view('user.top');
+
+        $thread_attensions = Thread::orderBy('num_of_comments','asc')
+        ->take(3)->get();
+    
+        return view('user.top')->with([
+            'thread_attensions' => $thread_attensions, 
+        ]);
     }
 }
--- resources/views/user/top.blade.php
+++ resources/views/user/top.blade.php
@@ -1,4 +1,22 @@
 @extends('templates.user.main')
-
+@section('style')
+    <link rel="stylesheet" href="{{ asset('css/user/top.css') }}">
+@endsection
 @section('contents')
+    <div class="topic1">
+        <h2>注目のスレッド</h2>
+    </div>
+    <div class="topic_body">
+        @forelse ($thread_attensions as $thread_attension)
+            <div class="comment-box">
+                <a href="{{ route('user.thread',['thread' => $thread_attension->id])}}">{{ $thread_attension->name }}</a>
+                <p>{{ Str::limit($thread_attension->details,200,'…')}}</p>
+                <p>{{ $thread_attension->created_at->format('Y年m月d日') }}</p>
+            </div>
+        @empty
+        @endforelse
+        <div>
+            {{-- {{ $thread_attensions->links('vendor.pagination.semantic-ui') }} --}}
+        </div>
+    </div>
 @endsection
